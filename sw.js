296d42e6cf8f5fe5c9519daa9d49494bc5f789c2
Rutvik Patel <99464586+rutvikpaatel@users.noreply.github.com>
2025-05-13 23:20:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,23 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+const CACHE_NAME = 'gst-calculator-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  // Add other assets like CSS or JS files if needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // basic cache-less fetch
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
